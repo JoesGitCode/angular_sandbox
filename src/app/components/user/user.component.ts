1dd302f4d8c7ef64a5a43117c0f5a9ab5c130264
JoesGitCode <jsyoung0891@gmail.com>
2019-10-18 23:46:32 +0100

    added typescript notes and implemented interface

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,51 @@
 import { Component } from "@angular/core";
+import { User } from "../../models/User";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent {}
+export class UserComponent {
+  // using an interface for the user to make it more modular. put interface in another file
+  user: User;
+
+  constructor() {
+    this.user = {
+      firstName: "User",
+      lastName: "One",
+      age: 100,
+      address: {
+        street: "Princes Street",
+        city: "Edinburgh",
+        country: "Scotland"
+      }
+    };
+  }
+
+  userSpeak() {
+    return `Hello, my name is ${this.user.firstName} ${this.user.lastName}`;
+  }
+
+  addNum(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+}
+
+// Examples of Typescript types
+
+// firstName: string;
+// lastName: string;
+// age: number;
+// address;
+// anyType: any;
+// hasBeard: boolean;
+// numberArray: number[];
+// stringArray: string[];
+// mixedArray: any[];
+
+// this.anyType = 45;
+// this.hasBeard = true;
+// this.numberArray = [1, 2, 3];
+// this.stringArray = ["1", "2"];
+// this.mixedArray = ["1", 2];
