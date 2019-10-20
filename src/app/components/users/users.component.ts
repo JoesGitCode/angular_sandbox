55437123d028ec8248e4dad00b83b5c7df727225
JoesGitCode <jsyoung0891@gmail.com>
2019-10-20 23:03:35 +0100

    added conditionals  to the users array

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../models/User";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.sass']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.sass"]
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // can wrap this in a set timeout to mimic loading data from and external source
+    // setTimeout(() => {
+    this.users = [
+      {
+        firstName: "Steve",
+        lastName: "Austin",
+        age: 50,
+        address: {
+          street: "Princes Street",
+          city: "Edinburgh",
+          country: "Scotland"
+        }
+      },
+      {
+        firstName: "Shawn",
+        lastName: "Michaels",
+        age: 60,
+        address: {
+          street: "Gwanak-gu",
+          city: "Seoul",
+          country: "Korea"
+        }
+      },
+      {
+        firstName: "User",
+        lastName: "One",
+        age: 100,
+        address: {
+          street: "Main Street",
+          city: "Rome",
+          country: "Italy"
+        }
+      }
+    ];
+
+    this.loaded = true;
+    // }, 2000);
+
+    this.showExtended = false;
+
+    this.addUser({
+      firstName: "User",
+      lastName: "Two",
+      age: 100
+    });
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,12 @@
 export interface User {
   firstName: string;
   lastName: string;
-  age: number;
-  address: {
-    street: string;
-    city: string;
-    country: string;
+  age?: number;
+  address?: {
+    street?: string;
+    city?: string;
+    country?: string;
   };
 }
+
+// the question mark makes them optional when utilizing the interface
